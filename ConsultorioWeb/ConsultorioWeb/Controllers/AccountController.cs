75b8b5c03c32c612602a551b41e80d6386a3013b
Leandro Rowies <lrowies@accountsiq.com>
2018-10-23 12:46:46 +0100

    Cambiar color de fondo y letra

--- ConsultorioWeb/ConsultorioWeb/Controllers/AccountController.cs
+++ ConsultorioWeb/ConsultorioWeb/Controllers/AccountController.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Globalization;
-using System.Linq;
-using System.Security.Claims;
-using System.Threading.Tasks;
-using System.Web;
-using System.Web.Mvc;
+﻿using ConsultorioWeb.Models;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
-using ConsultorioWeb.Models;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
 
 namespace ConsultorioWeb.Controllers
 {
