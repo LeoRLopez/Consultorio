7cc7228b3fa136e6d230af24edbaa13c3c3f2e4c
Leonardo Rafael LÓPEZ <leonardo_lrl@ca.frre.utn.edu.ar>
2018-11-06 23:27:07 -0300

    nueva ventana Bancos, faltaria agregar el bindingsource

---
+++ ConsultorioDesktop/Consultorio/Forms/Bancos.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace Consultorio.Forms
+{
+    partial class Bancos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBuscar = new Telerik.WinControls.UI.RadButton();
+            this.lblBuscar = new Telerik.WinControls.UI.RadLabel();
+            this.txtBoxBuscar = new Telerik.WinControls.UI.RadTextBox();
+            this.btnEliminar = new Telerik.WinControls.UI.RadButton();
+            this.btnEditar = new Telerik.WinControls.UI.RadButton();
+            this.dgvSegurosMedicos = new System.Windows.Forms.DataGridView();
+            this.btnVolver = new Telerik.WinControls.UI.RadButton();
+            this.btnAgregar = new Telerik.WinControls.UI.RadButton();
+            ((System.ComponentModel.ISupportInitialize)(this.btnBuscar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lblBuscar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtBoxBuscar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnEliminar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnEditar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSegurosMedicos)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnVolver)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnAgregar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.btnBuscar.Location = new System.Drawing.Point(429, 64);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(157, 24);
+            this.btnBuscar.TabIndex = 20;
+            this.btnBuscar.Text = "Buscar";
+            // 
+            // lblBuscar
+            // 
+            this.lblBuscar.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.lblBuscar.Location = new System.Drawing.Point(486, 12);
+            this.lblBuscar.Name = "lblBuscar";
+            this.lblBuscar.Size = new System.Drawing.Size(45, 19);
+            this.lblBuscar.TabIndex = 19;
+            this.lblBuscar.Text = "Buscar:";
+            // 
+            // txtBoxBuscar
+            // 
+            this.txtBoxBuscar.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.txtBoxBuscar.Location = new System.Drawing.Point(429, 37);
+            this.txtBoxBuscar.Name = "txtBoxBuscar";
+            this.txtBoxBuscar.Size = new System.Drawing.Size(157, 21);
+            this.txtBoxBuscar.TabIndex = 18;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.btnEliminar.Location = new System.Drawing.Point(429, 164);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(157, 25);
+            this.btnEliminar.TabIndex = 17;
+            this.btnEliminar.Text = "Eliminar";
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.btnEditar.Location = new System.Drawing.Point(429, 136);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(157, 25);
+            this.btnEditar.TabIndex = 16;
+            this.btnEditar.Text = "Editar";
+            // 
+            // dgvSegurosMedicos
+            // 
+            this.dgvSegurosMedicos.AllowUserToAddRows = false;
+            this.dgvSegurosMedicos.AllowUserToDeleteRows = false;
+            this.dgvSegurosMedicos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvSegurosMedicos.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(233)))), ((int)(((byte)(252)))), ((int)(((byte)(249)))));
+            this.dgvSegurosMedicos.Location = new System.Drawing.Point(12, 12);
+            this.dgvSegurosMedicos.Name = "dgvSegurosMedicos";
+            this.dgvSegurosMedicos.ReadOnly = true;
+            this.dgvSegurosMedicos.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvSegurosMedicos.Size = new System.Drawing.Size(409, 308);
+            this.dgvSegurosMedicos.TabIndex = 15;
+            // 
+            // btnVolver
+            // 
+            this.btnVolver.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.btnVolver.Location = new System.Drawing.Point(450, 296);
+            this.btnVolver.Name = "btnVolver";
+            this.btnVolver.Size = new System.Drawing.Size(110, 24);
+            this.btnVolver.TabIndex = 14;
+            this.btnVolver.Text = "Volver";
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.btnAgregar.Location = new System.Drawing.Point(429, 106);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(157, 25);
+            this.btnAgregar.TabIndex = 13;
+            this.btnAgregar.Text = "Agregar";
+            // 
+            // Bancos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(233)))), ((int)(((byte)(240)))), ((int)(((byte)(249)))));
+            this.ClientSize = new System.Drawing.Size(598, 332);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.lblBuscar);
+            this.Controls.Add(this.txtBoxBuscar);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.dgvSegurosMedicos);
+            this.Controls.Add(this.btnVolver);
+            this.Controls.Add(this.btnAgregar);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Bancos";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Bancos";
+            ((System.ComponentModel.ISupportInitialize)(this.btnBuscar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lblBuscar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtBoxBuscar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnEliminar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnEditar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSegurosMedicos)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnVolver)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnAgregar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Telerik.WinControls.UI.RadButton btnBuscar;
+        private Telerik.WinControls.UI.RadLabel lblBuscar;
+        private Telerik.WinControls.UI.RadTextBox txtBoxBuscar;
+        private Telerik.WinControls.UI.RadButton btnEliminar;
+        private Telerik.WinControls.UI.RadButton btnEditar;
+        private System.Windows.Forms.DataGridView dgvSegurosMedicos;
+        private Telerik.WinControls.UI.RadButton btnVolver;
+        private Telerik.WinControls.UI.RadButton btnAgregar;
+    }
+}
---
+++ ConsultorioDesktop/Consultorio/Forms/Bancos.cs
@@ -0,0 +1,69 @@
+﻿using Consultorio.Modelo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Consultorio.Forms
+{
+    public partial class Bancos : Form
+    {
+        public Bancos()
+        {
+            InitializeComponent();
+        }
+
+        private void Forma_Pago_Load(object sender, EventArgs e)
+        {
+            RefrescarGridView();
+        }
+
+        private void RefrescarGridView()
+        {
+            using (var entidades = new ClinicaEntities())
+            {
+                
+            }
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtBoxBuscar.Text))
+            {
+                // Retornar todos los registros porque no escribio nada para buscar
+                RefrescarGridView();
+                return;
+            }
+            using (var entidades = new ClinicaEntities())
+            {
+
+            }
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            // Crear Formulario para Añadir/Editar Registro
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            // Crear Formulario para Añadir/Editar Registro
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            // Chequear si no se esta usando se setea a TRUE la columna BajaLogica, 
+            // de lo contrario se muestra por pantalla un msj de que esta en uso
+        }
+
+        private void btnVolver_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
