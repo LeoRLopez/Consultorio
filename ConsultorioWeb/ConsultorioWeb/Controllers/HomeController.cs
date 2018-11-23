65dfef560fd3a9a81e10c4062886c1d198b6df96
Leandro Rowies <lrowies@accountsiq.com>
2018-11-23 11:01:44 +0000

    informacion del consultorio

--- ConsultorioWeb/ConsultorioWeb/Controllers/HomeController.cs
+++ ConsultorioWeb/ConsultorioWeb/Controllers/HomeController.cs
@@ -1,25 +1,34 @@
-﻿using System.Web.Mvc;
+﻿using ConsultorioWeb.Models;
+using System.Web.Mvc;
+using System.Linq;
 
 namespace ConsultorioWeb.Controllers
 {
     public class HomeController : Controller
     {
+        private ClinicaEntities db = new ClinicaEntities();
+
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
+            var infoConsultorio = db.InfoConsultorio.FirstOrDefault();
+            if (infoConsultorio != null)
+            {
+                ViewBag.Mision = infoConsultorio.Mision;
+                ViewBag.Vision = infoConsultorio.Vision;
+                ViewBag.Descripcion = infoConsultorio.Descripcion;
+            }
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
+            var infoConsultorio = db.InfoConsultorio.FirstOrDefault();
+            if (infoConsultorio != null)
+            {
+                ViewBag.Direccion = infoConsultorio.Dirección;
+                ViewBag.Telefono = infoConsultorio.Telefono;
+                ViewBag.Email = infoConsultorio.Email;
+            }
             return View();
         }
     }
--- ConsultorioWeb/ConsultorioWeb/Controllers/InfoConsultorioController.cs
+++ ConsultorioWeb/ConsultorioWeb/Controllers/InfoConsultorioController.cs
@@ -17,49 +17,11 @@ namespace ConsultorioWeb.Controllers
         // GET: InfoConsultorio
         public ActionResult Index()
         {
-            return View(db.InfoConsultorio.ToList());
-        }
-
-        // GET: InfoConsultorio/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            InfoConsultorio infoConsultorio = db.InfoConsultorio.Find(id);
-            if (infoConsultorio == null)
-            {
-                return HttpNotFound();
-            }
-            return View(infoConsultorio);
-        }
-
-        // GET: InfoConsultorio/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: InfoConsultorio/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "id,NombreConsultorio,Dirección,Telefono,Email,DiasyHorariosAtencion,Web,Mision,Vision,Descripcion")] InfoConsultorio infoConsultorio)
-        {
-            if (ModelState.IsValid)
-            {
-                db.InfoConsultorio.Add(infoConsultorio);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-
-            return View(infoConsultorio);
+            return RedirectToAction("Edit");
         }
 
         // GET: InfoConsultorio/Edit/5
-        public ActionResult Edit(int? id)
+        public ActionResult Edit(int? id = 1)
         {
             if (id == null)
             {
@@ -87,32 +49,6 @@ namespace ConsultorioWeb.Controllers
                 return RedirectToAction("Index");
             }
             return View(infoConsultorio);
-        }
-
-        // GET: InfoConsultorio/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            InfoConsultorio infoConsultorio = db.InfoConsultorio.Find(id);
-            if (infoConsultorio == null)
-            {
-                return HttpNotFound();
-            }
-            return View(infoConsultorio);
-        }
-
-        // POST: InfoConsultorio/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            InfoConsultorio infoConsultorio = db.InfoConsultorio.Find(id);
-            db.InfoConsultorio.Remove(infoConsultorio);
-            db.SaveChanges();
-            return RedirectToAction("Index");
         }
 
         protected override void Dispose(bool disposing)
